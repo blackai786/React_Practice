fa6399bba61311e46701c00e4b69a12ecdedc9c8
blackai786 <sahilshaikh120381718@gmail.com>
2026-07-02 10:14:45 +0530

    React pratice first day

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import "./App.css";
+import ProductTab from "./Product_tab.jsx";
+
+function App() {
+  return (
+    <ProductTab/>
+  );
+}
+
+export default App
---
+++ src/Product_tab.jsx
@@ -0,0 +1,13 @@
+import Product from "./Product.jsx";
+
+function ProductTabs() {
+  return (
+    <>
+      <Product />
+      <Product />
+      <Product />
+    </>
+  );
+}
+
+export default ProductTabs;
